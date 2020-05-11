a2860aebd8a43f1030920f36e6eb1123b168b545
imdhemy <imdhemy@gmail.com>
2020-05-11 11:34:02 +0200

    Implement dynamic array

--- main/dynamic_and_static_arrays/DynamicArray.java
+++ main/dynamic_and_static_arrays/DynamicArray.java
@@ -6,4 +6,116 @@ package dynamic_and_static_arrays;
 @SuppressWarnings("unchecked")
 public class DynamicArray<T> {
 
+  /**
+   * internal array
+   */
+  private T[] list;
+
+  /**
+   * List size
+   */
+  private int size;
+
+  /**
+   * actual capacity
+   */
+  private int capacity;
+
+  /**
+   * DynamicArray Constructor
+   */
+  public DynamicArray() {
+    capacity = 1;
+    list = (T[]) new Object[capacity];
+    size = 0;
+  }
+
+
+  /**
+   * checks if list is empty
+   *
+   * @return {@code true} if size is zero
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * adds new element to the list
+   *
+   * @param element the element to be added
+   */
+  public void add(T element) {
+    if (size + 1 >= capacity) {
+      expandListCapacity();
+    }
+    list[size++] = element;
+  }
+
+  /**
+   * Expands internal list capacity
+   */
+  private void expandListCapacity() {
+    capacity *= 2;
+    T[] doubleSizedList = (T[]) new Object[capacity];
+    System.arraycopy(list, 0, doubleSizedList, 0, size);
+    list = doubleSizedList;
+  }
+
+  /**
+   * Get list size
+   *
+   * @return get list size
+   */
+  public int getSize() {
+    return size;
+  }
+
+  /**
+   * finds index of the specified element
+   *
+   * @param element element to search for
+   * @return return {-1} if element not found, and returns zero or more if found
+   */
+  public int indexOf(T element) {
+    for (int i = 0; i < size; i++) {
+      if (element == list[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * removes the specified element from the list
+   *
+   * @param element the element to be removed
+   * @return {@code true} if removed
+   */
+  public boolean remove(T element) {
+    int index = indexOf(element);
+    if (index == -1) {
+      return false;
+    }
+    removeAt(index);
+    return true;
+  }
+
+  /**
+   * removes the element at the specified index
+   *
+   * @param remIndex the index to be removed
+   */
+  private void removeAt(int remIndex) {
+    T[] newList = (T[]) new Object[capacity];
+    for (int index = 0, newIndex = 0; index < size; index++, newIndex++) {
+      if (index == remIndex) {
+        newIndex--;
+      } else {
+        newList[newIndex] = list[index];
+      }
+    }
+    size--;
+    list = newList;
+  }
 }
--- tests/dynamic_and_static_arrays/DynamicArrayTest.java
+++ tests/dynamic_and_static_arrays/DynamicArrayTest.java
@@ -1,5 +1,92 @@
 package dynamic_and_static_arrays;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class DynamicArrayTest {
 
+  @Test
+  public void test_it_creates_empty_list() {
+    DynamicArray<Integer> list = new DynamicArray<>();
+    assertTrue(list.isEmpty());
+  }
+
+  @Test
+  public void test_id_add_elements_to_list() {
+    DynamicArray<Integer> list = new DynamicArray<>();
+    list.add(1);
+    assertFalse(list.isEmpty());
+  }
+
+  @Test
+  public void test_it_can_get_list_size() {
+    DynamicArray<Integer> list = new DynamicArray<>();
+    for (int i = 0; i < 5; i++) {
+      list.add(i);
+    }
+    assertEquals(5, list.getSize());
+  }
+
+  @Test
+  public void test_it_expands_if_reaches_max_capacity() {
+    DynamicArray<Integer> list = new DynamicArray<>();
+    int size = 200;
+    for (int i = 0; i < size; i++) {
+      list.add(i);
+    }
+    assertEquals(size, list.getSize());
+  }
+
+  @Test
+  public void test_it_can_get_index_of_an_element() {
+    DynamicArray<String> list = new DynamicArray<>();
+    String[] elements = {"a", "b", "c"};
+    for (String element : elements) {
+      list.add(element);
+    }
+    assertEquals(-1, list.indexOf("z"));
+    assertEquals(0, list.indexOf("a"));
+    assertEquals(1, list.indexOf("b"));
+    assertEquals(2, list.indexOf("c"));
+  }
+
+  @Test
+  public void test_it_can_remove_an_element() {
+    DynamicArray<String> list = new DynamicArray<>();
+    String[] elements = {"a", "b", "c"};
+    for (String element : elements) {
+      list.add(element);
+    }
+    assertEquals(3, list.getSize());
+    for (int i = 2; i >= 0; i--) {
+      assertTrue(list.remove(elements[i]));
+      assertEquals(i, list.getSize());
+    }
+  }
+
+  @Test
+  public void test_get_false_if_it_removes_unlisted_element() {
+    DynamicArray<String> list = new DynamicArray<>();
+    String[] elements = {"a", "b", "c"};
+    for (String element : elements) {
+      list.add(element);
+    }
+    assertEquals(3, list.getSize());
+    assertFalse(list.remove("z"));
+    assertEquals(3, list.getSize());
+  }
+
+  @Test
+  public void test_get_negative_index_if_element_was_removed() {
+    DynamicArray<String> list = new DynamicArray<>();
+    String[] elements = {"a", "b", "c"};
+    for (String element : elements) {
+      list.add(element);
+    }
+    list.remove("a");
+    assertEquals(-1, list.indexOf("a"));
+  }
 }
