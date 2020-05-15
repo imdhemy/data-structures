96f88801b9fd4cc723ed3031e0c37912b4c856a1
imdhemy <imdhemy@gmail.com>
2020-05-15 19:12:18 +0200

    Implement singly linked list class

--- main/linked_list/SinglyLinkedList.java
+++ main/linked_list/SinglyLinkedList.java
@@ -1,5 +1,277 @@
 package linked_list;
 
-public class SinglyLinkedList {
+import java.util.ArrayList;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class SinglyLinkedList<T> {
+
+  /**
+   * list size
+   */
+  private int size = 0;
+  /**
+   * Head node
+   */
+  private Node<T> head;
+
+  /**
+   * Checks if the list is empty
+   *
+   * @return {@code true} if size is zero
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * @param element add new element to the list
+   */
+  public void add(T element) {
+    Node<T> node = new Node<>(element);
+    if (isEmpty()) {
+      head = node;
+    } else {
+      getTailNode().setNext(node);
+    }
+    size++;
+  }
+
+  /**
+   * @return the list size
+   */
+  public int getSize() {
+    return size;
+  }
+
+  /**
+   * @return the list head node
+   */
+  public T getHead() {
+    return head.getData();
+  }
+
+  /**
+   * @return the tail node
+   */
+  private Node<T> getTailNode() {
+    Node<T> node = head;
+    while (node.getNext() != null) {
+      node = node.getNext();
+    }
+    return node;
+  }
+
+  /**
+   * @return the last element in the list
+   */
+  public T getTail() {
+    return getTailNode().getData();
+  }
+
+  /**
+   * @param element the element to be added to the beginning
+   */
+  public void addToBeginning(T element) {
+    Node<T> node = new Node(element);
+    node.setNext(head);
+    head = node;
+    size++;
+  }
+
+  /**
+   * @param element the element to search for
+   * @return the index of the specified element
+   */
+  public int indexOf(T element) {
+    Node<T> node = head;
+    int index = 0;
+
+    while (node != null) {
+      if (node.getData() == element) {
+        return index;
+      }
+      index++;
+      node = node.getNext();
+    }
+
+    return -1;
+  }
+
+  /**
+   * @param index the index to look for
+   * @return the element at the specified index
+   * @throws Exception if the index was out of bounds
+   */
+  public T get(int index) throws Exception {
+    Node<T> node = head;
+    int i = 0;
+    while (node != null) {
+      if (i == index) {
+        return node.getData();
+      }
+      node = node.getNext();
+      i++;
+    }
+    throw new Exception(String.format("%d is out of bounds of %d", index, size - 1));
+  }
+
+  /**
+   * Clears the list
+   */
+  public void clear() {
+    Node<T> node = head;
+    while (node != null) {
+      Node next = node.getNext();
+      node.setData(null);
+      node.setNext(null);
+      node = next;
+    }
+    head = null;
+    size = 0;
+  }
+
+  /**
+   * removes the head node
+   */
+  private void removeHead() {
+    if (!isEmpty()) {
+      head.setNext(head.getNext());
+      head = null;
+      size--;
+    }
+  }
+
+  /**
+   * removes the tail node
+   */
+  private void removeTail() {
+    if (!isEmpty()) {
+      Node node = head;
+      while (node.getNext().getNext() != null) {
+        node = node.getNext();
+      }
+      node.setNext(null);
+      size--;
+    }
+  }
+
+  /**
+   * removes the element at the specified index
+   *
+   * @param index the index to remove at
+   * @throws Exception if the index was out of bounds
+   */
+  public void removeAt(int index) throws Exception {
+    if (index >= size || index < 0) {
+      throw new Exception(String.format("%d is out of bounds of %d", index, size - 1));
+    }
+    if (index == 0) {
+      removeHead();
+    } else if (index == size - 1) {
+      removeTail();
+    } else {
+      Node prev = head;
+      removeAt(index, prev);
+    }
+  }
+
+  /**
+   * @param index the index to remove at
+   * @param head  the list head node
+   */
+  private void removeAt(int index, Node head) {
+    for (int i = 0; i < index - 1; i++) {
+      head = head.getNext();
+    }
+    head.setNext(head.getNext().getNext());
+    size--;
+  }
+
+  /**
+   * removes the specified element
+   *
+   * @param element the element to be removed
+   * @throws Exception if the element was not found
+   */
+  public void remove(T element) throws Exception {
+    int index = indexOf(element);
+    if (index < 0) {
+      throw new Exception(String.format("%s not found in list %s", element.toString(), toString()));
+    } else {
+      removeAt(index);
+    }
+  }
+
+  @Override
+  public String toString() {
+    ArrayList<T> elements = new ArrayList<>();
+    Node<T> node = head;
+    while (node != null) {
+      elements.add(node.getData());
+      node = node.getNext();
+    }
+    return elements.toString();
+  }
+
+  /**
+   * Node class
+   *
+   * @param <T>
+   */
+  static class Node<T> {
+
+    /**
+     * Node data
+     */
+    private T data;
+
+    /**
+     * Next element
+     */
+    private Node next;
+
+    /**
+     * Node constructor
+     *
+     * @param data node value
+     */
+    public Node(T data) {
+      this.data = data;
+      next = null;
+    }
+
+    /**
+     * @return get node value
+     */
+    public T getData() {
+      return data;
+    }
+
+    /**
+     * @param o set the node value
+     */
+    public void setData(Object o) {
+      data = (T) o;
+    }
+
+    /**
+     * @return the next node
+     */
+    public Node getNext() {
+      return next;
+    }
+
+    /**
+     * @param node set the next node
+     */
+    public void setNext(Node<T> node) {
+      next = node;
+    }
+
+    @Override
+    public String toString() {
+      return data.toString();
+    }
+  }
 
 }
--- tests/linked_list/SinglyLinkedListTest.java
+++ tests/linked_list/SinglyLinkedListTest.java
@@ -1,5 +1,195 @@
 package linked_list;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class SinglyLinkedListTest {
 
+  private SinglyLinkedList<String> list;
+
+  @Before
+  public void setUp() {
+    list = new SinglyLinkedList<>();
+  }
+
+  @Test
+  public void it_can_create_empty_list() {
+    assertTrue(list.isEmpty());
+  }
+
+  @Test
+  public void it_can_add_element_to_list_tail() {
+    list.add("A");
+    assertFalse(list.isEmpty());
+  }
+
+  @Test
+  public void it_can_get_list_size() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals(3, list.getSize());
+  }
+
+  @Test
+  public void it_can_get_the_value_of_the_head_element() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("a", list.getHead());
+  }
+
+  @Test
+  public void it_can_get_the_value_of_the_tail_element() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("c", list.getTail());
+  }
+
+  @Test
+  public void it_can_add_element_to_beginning_of_list() {
+    list.add("a");
+    list.add("b");
+    list.addToBeginning("c");
+    assertEquals("c", list.getHead());
+  }
+
+  @Test
+  public void it_can_get_string_representation() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("[a, b, c]", list.toString());
+  }
+
+  @Test
+  public void it_can_search_for_an_element() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals(2, list.indexOf("c"));
+  }
+
+  @Test
+  public void it_can_get_value_by_index() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("b", list.get(1));
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_if_index_was_out_of_bounds() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("b", list.get(7));
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_if_index_was_a_negative_value() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals("b", list.get(-1));
+  }
+
+  @Test
+  public void it_can_search_for_null_element() {
+    list.add("a");
+    list.add(null);
+    list.add("c");
+    assertEquals(1, list.indexOf(null));
+  }
+
+  @Test
+  public void it_can_clear_list() {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    assertEquals(3, list.getSize());
+
+    list.clear();
+
+    assertTrue(list.isEmpty());
+    assertEquals(-1, list.indexOf("a"));
+    assertEquals(0, list.getSize());
+  }
+
+  @Test
+  public void it_can_remove_the_head_element() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    String head = list.getHead();
+    list.removeAt(0);
+    assertEquals(-1, list.indexOf(head));
+  }
+
+  @Test
+  public void it_can_remove_the_tail_element() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    String tail = list.getTail();
+    list.removeAt(2);
+    assertEquals(-1, list.indexOf(tail));
+  }
+
+  @Test
+  public void it_can_remove_at_a_particular_index_from_the_list() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.removeAt(1);
+    assertEquals(-1, list.indexOf("b"));
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_on_removal_if_index_was_out_of_bounds() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.removeAt(3);
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_on_removal_if_index_was_negative() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.removeAt(-1);
+  }
+
+  @Test
+  public void it_can_remove_arbitrary_element_from_the_list() throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.remove("a");
+    assertEquals(-1, list.indexOf("a"));
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_on_removal_of_arbitrary_element_if_element_was_not_found()
+      throws Exception {
+    list.add("a");
+    list.add("b");
+    list.add("c");
+    list.remove("z");
+  }
+
+  @Test
+  public void it_can_remove_null_element() throws Exception {
+    list.add(null);
+    list.add("a");
+    assertEquals(0, list.indexOf(null));
+    list.remove(null);
+    assertEquals(-1, list.indexOf(null));
+  }
 }
