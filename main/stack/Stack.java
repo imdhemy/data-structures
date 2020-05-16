782cf65c4b09632f3cb6749315772df78788e4b2
imdhemy <imdhemy@gmail.com>
2020-05-16 05:36:24 +0200

    Implement Stack class

--- main/stack/Stack.java
+++ main/stack/Stack.java
@@ -1,5 +1,62 @@
 package stack;
 
-public class Stack {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Stack<T> implements Iterable {
+
+  private int size;
+
+  private ArrayList<T> list;
+
+  public Stack() {
+    size = 0;
+    list = new ArrayList<>();
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public void push(T element) {
+    list.add(element);
+    size++;
+  }
+
+  public T pop() throws Exception {
+    if (isEmpty()) {
+      throw new Exception("The stack is empty");
+    }
+    return list.remove(--size);
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  @Override
+  public String toString() {
+    return list.toString();
+  }
+
+  @Override
+  public Iterator iterator() {
+    return new Iterator() {
+      private int pointer = size - 1;
+
+      @Override
+      public boolean hasNext() {
+        return pointer >= 0;
+      }
+
+      @Override
+      public Object next() {
+        return list.get(pointer--);
+      }
+    };
+  }
+
+  public int indexOf(T element) {
+    return list.indexOf(element);
+  }
 }
--- tests/stack/StackTest.java
+++ tests/stack/StackTest.java
@@ -1,5 +1,123 @@
 package stack;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StackTest {
 
+  private Stack<String> stack;
+
+  @Before
+  public void setUp() {
+    stack = new Stack<>();
+  }
+
+  @Test
+  public void it_can_create_empty_stack() {
+    assertTrue(stack.isEmpty());
+  }
+
+  @Test
+  public void it_can_push_elements_stack() {
+    stack.push("a");
+    stack.push("b");
+    stack.push("c");
+    assertFalse(stack.isEmpty());
+  }
+
+  @Test
+  public void it_can_convert_stack_to_string() {
+    stack.push("a");
+    stack.push("b");
+    stack.push("c");
+    assertEquals(Arrays.asList("a", "b", "c").toString(), stack.toString());
+  }
+
+  @Test
+  public void it_can_pop_element_from_stack() throws Exception {
+    List<String> items = Arrays.asList("a", "b", "c");
+
+    for (String i : items) {
+      stack.push(i);
+    }
+
+    ListIterator iterator = items.listIterator(items.size());
+    while (iterator.hasPrevious()) {
+      assertEquals(iterator.previous(), stack.pop());
+    }
+  }
+
+  @Test
+  public void it_can_get_stack_size() {
+    List<String> items = Arrays.asList("a", "b", "c");
+
+    for (String i : items) {
+      stack.push(i);
+    }
+
+    assertEquals(items.size(), stack.getSize());
+  }
+
+  @Test
+  public void it_can_iterate_elements() {
+    List<String> items = Arrays.asList("a", "b", "c");
+
+    for (String i : items) {
+      stack.push(i);
+    }
+
+    ArrayList<String> list = new ArrayList<>();
+    for (Object i : stack) {
+      list.add((String) i);
+    }
+
+    ListIterator iterator = items.listIterator(items.size());
+    Iterator stackIterator = stack.iterator();
+
+    while (iterator.hasPrevious() && stackIterator.hasNext()) {
+      assertEquals(iterator.previous(), stackIterator.next());
+    }
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_on_popping_from_empty_stack() throws Exception {
+    stack.pop();
+  }
+
+  @Test(expected = Exception.class)
+  public void it_throws_exception_on_popping_from_empty_stack_2() throws Exception {
+    stack.push("a");
+    stack.push("b");
+    stack.push("c");
+
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+  }
+
+  @Test
+  public void it_can_search_for_element() {
+    stack.push("a");
+    stack.push("b");
+    stack.push("c");
+    assertEquals(1, stack.indexOf("b"));
+  }
+
+  @Test
+  public void it_can_search_for_element_2() {
+    stack.push("a");
+    stack.push("b");
+    stack.push("c");
+    assertEquals(-1, stack.indexOf("z"));
+  }
 }
