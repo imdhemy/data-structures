2692a1a4eee138897296103c563b57b3eedcfb3e
imdhemy <imdhemy@gmail.com>
2020-05-24 01:54:08 +0200

    Implement Queue

--- main/queue/Queue.java
+++ main/queue/Queue.java
@@ -1,5 +1,39 @@
 package queue;
 
-public class Queue {
+import dynamic_and_static_array.DynamicArray;
 
+public class Queue<T> {
+
+  private DynamicArray<T> list = new DynamicArray<T>();
+
+  public boolean isEmpty() {
+    return list.isEmpty();
+  }
+
+  public int getSize() {
+    return list.getSize();
+  }
+
+  public void enqueue(T element) {
+    list.add(element);
+  }
+
+  public T dequeue() throws Exception {
+    return list.removeAt(0);
+  }
+
+  public boolean contains(T a) {
+    return list.indexOf(a) >= 0;
+  }
+
+  public T peek() {
+    return list.get(0);
+  }
+
+  public void clear() throws Exception {
+    int size = list.getSize();
+    for (int i = 0; i < size; i++) {
+      dequeue();
+    }
+  }
 }
--- tests/queue/QueueTest.java
+++ tests/queue/QueueTest.java
@@ -1,5 +1,72 @@
 package queue;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.stream.IntStream;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QueueTest {
 
+  private Queue<String> queue;
+
+  @Before
+  public void setUp() {
+    queue = new Queue<>();
+  }
+
+  @Test
+  public void it_can_create_empty_list() {
+    assertTrue(queue.isEmpty());
+  }
+
+  @Test
+  public void it_can_get_the_list_size() {
+    assertEquals(0, queue.getSize());
+  }
+
+  @Test
+  public void it_can_enqueue_elements_to_the_list() {
+    queue.enqueue("a");
+    queue.enqueue("b");
+    queue.enqueue("c");
+    assertEquals(3, queue.getSize());
+    assertFalse(queue.isEmpty());
+  }
+
+  @Test
+  public void it_can_dequeue_elements_from_the_list() throws Exception {
+    String[] elements = {"a", "b", "c"};
+    IntStream.range(0, elements.length).forEach(i -> queue.enqueue(elements[i]));
+    for (String element : elements) {
+      assertEquals(element, queue.dequeue());
+    }
+  }
+
+  @Test
+  public void it_can_search_for_an_element() throws Exception {
+    assertFalse(queue.contains("a"));
+    queue.enqueue("a");
+    assertTrue(queue.contains("a"));
+    queue.dequeue();
+    queue.enqueue("a");
+  }
+
+  @Test
+  public void it_can_peek_the_list() {
+    String[] elements = {"a", "b", "c"};
+    IntStream.range(0, elements.length).forEach(i -> queue.enqueue(elements[i]));
+    IntStream.range(0, elements.length).forEach(i -> assertEquals(elements[0], queue.peek()));
+  }
+
+  @Test
+  public void it_can_clear_the_list() throws Exception {
+    String[] elements = {"a", "b", "c"};
+    IntStream.range(0, elements.length).forEach(i -> queue.enqueue(elements[i]));
+    assertEquals(elements.length, queue.getSize());
+    queue.clear();
+    assertEquals(0, queue.getSize());
+  }
 }
